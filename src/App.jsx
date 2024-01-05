2392a2c7ee7092fabeecd7af3d19217cc88d6317
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2024-01-05 22:15:45 +0600

    design location,humidity,feels like section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 
 import './App.css'
-import axios from 'axios'
 import WeatherApp from './Page/WeatherApp'
 function App() {
 
-  const url=`https://api.openweathermap.org/data/2.5/weather?q=London&appid=097c9a5daccb743c11eafa92e4b6d383`
+
 
   return (
     <>
--- src/Page/WeatherApp.jsx
+++ src/Page/WeatherApp.jsx
@@ -1,26 +1,36 @@
-import './WeatherApp.css'
+import "./WeatherApp.css";
+import axios from "axios";
 const WeatherApp = () => {
-    return (
-        <div className='main'> 
-            <div className="container">
-               <div className="top">
-                    <div className="location">
-                        <p>London</p>
-                    </div>
-                    <div>
-                        <h1>60C</h1>
-                    </div>
-                    <div className="description"><p>Clouds</p></div>
-               </div>
-               <div className="bottom">
-                      <div className="feels"><p>60C</p></div>
-                      <div className="humidity"><p>20%</p></div>
-                      <div className="wind">12MPH</div>
-               </div>
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=097c9a5daccb743c11eafa92e4b6d383`;
+
+  return (
+    <div>
+      <div className="main font-semibold">
+        <div className="max-w-[700px] mx-auto px-4 relative top-[10%] flex justify-between flex-col border h-[90%]">
+          <div className=" w-[100%] mx-4">
+            <div className="location">
+              <p className="text-2xl">London</p>
             </div>
-            
+              <div>
+                <h1 className="text-5xl">60C</h1>
+              </div>
+              <div className="mt-10 description">
+                <p className="text-2xl ">Clouds</p>
+              </div>
+          </div>
+          <div className="bottom flex justify-evenly w-[100%] text-center p-4 my-4 mx-auto rounded-xl bg-[#ffffff33] ">
+            <div className="feels">
+              <p className="text-2xl">60C</p>
+            </div>
+            <div className="humidity">
+              <p className="text-2xl">20%</p>
+            </div>
+            <div className="wind">12MPH</div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default WeatherApp;
